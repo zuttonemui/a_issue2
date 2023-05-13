7a3347b2fd32d335610216027b1e4f561e2c7195
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-05-13 07:16:59 +0000

    [Finish]応用課題3

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,19 @@
+class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+  
+  def book_params
+    params.require(:book_comment).permit(:comment)
+  end
+  
+end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = User.find(@book.user_id)
     @bookn = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
