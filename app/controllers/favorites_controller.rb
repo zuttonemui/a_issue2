28f9a7790a2287b35ebb387db51bca1dd13831e3
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-05-13 07:27:23 +0000

    [Finish]応用課題3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,7 +8,7 @@ class FavoritesController < ApplicationController
   
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
     redirect_back(fallback_location: root_path)
   end
