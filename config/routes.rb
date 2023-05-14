8e4ada55ae3a302630836a867541e6ecd2c5a5fb
zuttonemui <130944391+zuttonemui@users.noreply.github.com>
2023-05-14 05:46:19 +0000

    [Finish]応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-
+  get "search" => "searches#search_result"
 end
